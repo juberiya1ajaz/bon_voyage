63051e3f3873ff924eb77a320032999ddc5c72fa
Harshal0902 <harshalraikwar07@gmail.com>
2022-06-05 14:20:25 +0530

    ✨ Add Home page

--- src/container/Main.js
+++ src/container/Main.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar'
+import Navbar from '../components/Navbar'
 import Home from '../pages/Home';
 
 export default function Main() {
